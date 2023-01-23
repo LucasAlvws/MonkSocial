41228f567c0c8812f94d48521b90bae1628b01a3
LucasAlvws <sociallucasalves@gmail.com>
2023-01-23 12:49:20 -0300

    start

---
+++ Rede/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+#user:
+''' - nome char 50
+    - senha password
+    - email email
+    - foto perfil img
+    - foto capa img
+    - info char 500
+'''
+
+#publicaçoes
+''' - publicao texto
+    - n° likes
+    - comentarios
+'''
